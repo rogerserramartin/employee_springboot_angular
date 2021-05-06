e5edb455889c653a622db24ce8e88c8ff12cbe4f
rogersm92 <roger.serra92@gmail.com>
2021-05-06 17:01:57 +0200

    app running

--- employeemanagerapp/src/app/employee.service.ts
+++ employeemanagerapp/src/app/employee.service.ts
@@ -1,34 +1,28 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Employee } from './employee';
 import { environment } from 'src/environments/environment';
 
-
-@Injectable({
-    providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
 export class EmployeeService {
-    private apiServerUrl = environment.apiBaseUrl;
+  private apiServerUrl = environment.apiBaseUrl;
 
+  constructor(private http: HttpClient){}
 
-constructor(private http: HttpClient){ }
-
-public getEmployees(): Observable<Employee[]> {
+  public getEmployees(): Observable<Employee[]> {
     return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
-}
+  }
 
-public addEmployee(employee: Employee): Observable<Employee> {
+  public addEmployee(employee: Employee): Observable<Employee> {
     return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
-}
+  }
 
-public updateEmployee(employee: Employee): Observable<Employee> {
+  public updateEmployee(employee: Employee): Observable<Employee> {
     return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
-}
+  }
 
-public deleteEmployee(employeeId: number): Observable<void> {
+  public deleteEmployee(employeeId: number): Observable<void> {
     return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
-}
-
-
+  }
 }
--- employeemanagerapp/src/app/employee.ts
+++ employeemanagerapp/src/app/employee.ts
@@ -1,10 +1,9 @@
 export interface Employee {
-    id: number,
-    name: string, 
-    email: string,
-    jobTitle: string,
-    phone: string,
-    imageUrl: string,
-    employeeCode: string
-    salary: number
+  id: number;
+  name: string;
+  email: string;
+  jobTitle: string;
+  phone: string;
+  imageUrl: string;
+  employeeCode: string;
 }
